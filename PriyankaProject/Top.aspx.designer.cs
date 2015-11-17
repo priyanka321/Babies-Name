fe5b2985a7f1ec9dc911c535ebb9cc6616fbd060
anjali <anjali>
2015-11-17 12:51:46 +0600

    c

--- PriyankaProject/Top.aspx.designer.cs
+++ PriyankaProject/Top.aspx.designer.cs
@@ -11,7 +11,16 @@ namespace PriyankaProject
 {
 
 
-    public partial class WebForm2
+    public partial class Top
     {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
